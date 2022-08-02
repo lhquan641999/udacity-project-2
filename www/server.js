a8952b6b8f3e9a94667767c8d9bad3ced5c2e174
QuanLH20 <lehoangquan.com>
2022-08-02 12:19:36 +0700

    update png result

---
+++ www/server.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+const util_1 = require("./util/util");
+const express_1 = __importDefault(require("express"));
+(() => __awaiter(this, void 0, void 0, function* () {
+    // Init the Express application
+    const app = express_1.default();
+    // Set the network port
+    const port = process.env.PORT || 8082;
+    // Use the body parser middleware for post requests
+    app.use(body_parser_1.default.json());
+    // https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWFsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60
+    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    app.get("/filteredimage/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        let { image_url } = req.query;
+        let Img = yield util_1.filterImageFromURL(image_url);
+        if (!image_url) {
+            return res.status(404)
+                .send(`URL is required`);
+        }
+        return res.status(200).sendFile(Img, () => { });
+    }));
+    // Start the Server
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
